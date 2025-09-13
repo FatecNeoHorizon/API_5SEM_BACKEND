136865004c169d16dfe0e71a8653523140522687
cesarpelogia <pelogia.cesar@hotmail.com>
2025-09-12 22:01:33 -0300

    chore(#2): Adiciona configurações de build e dependências
    - Atualiza o pom.xml para incluir as dependências necessárias (Spring Web, Spring Data JPA, Postgre, Lombok).
    
    - Adiciona a configuração do banco de dados no application.properties.
    
    - Cria o arquivo .gitignore para ignorar arquivos desnecessários.
    
    - Adiciona .env.example para direcionar próximos desenvolvedores

---
+++ api/src/test/java/com/neohorizon/api/NeohorizonApplicationTests.java
@@ -0,0 +1,12 @@
+package com.neohorizon.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeohorizonApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
