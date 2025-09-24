dfc79d81656a6d3fde3f1f85b0bef038a3a73883
cesarpelogia <pelogia.cesar@hotmail.com>
2025-09-23 21:30:45 -0300

    refactor(23): Corrigido nome da classe de serviço visando padronização

--- api/src/main/java/com/neohorizon/api/controller/IssueController.java
+++ api/src/main/java/com/neohorizon/api/controller/IssueController.java
@@ -10,28 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neohorizon.api.dto.IssueDTO.ProjectIssueCountDTO;
-import com.neohorizon.api.service.TotalIssueService;
+import com.neohorizon.api.service.IssueService;
 
 @RestController
 @RequestMapping("/issue")
 public class IssueController {
 
     @Autowired
-    private TotalIssueService totalIssueService;
+    private IssueService issueService;
 
     @GetMapping("/total")
     public ResponseEntity<Long> getTotalIssues() {
-        return ResponseEntity.ok(totalIssueService.getTotalIssues());
+        return ResponseEntity.ok(issueService.getTotalIssues());
     }
 
     @GetMapping("/todos-projetos")
     public ResponseEntity<List<ProjectIssueCountDTO>> getAllProjectIssues() {
-        return ResponseEntity.ok(totalIssueService.getAllProjectIssues());
+        return ResponseEntity.ok(issueService.getAllProjectIssues());
     }
 
     @GetMapping("/por-projeto/{projectId}")
     public ResponseEntity<List<ProjectIssueCountDTO>> getIssuesByProject(@PathVariable Long projectId) {
-        return ResponseEntity.ok(totalIssueService.getIssuesByProject(projectId));
+        return ResponseEntity.ok(issueService.getIssuesByProject(projectId));
     }
 
 }
