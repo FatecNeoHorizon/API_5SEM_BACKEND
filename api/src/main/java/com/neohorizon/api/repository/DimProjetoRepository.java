8dd9608c6bea73952f7eca0cd033d9a52db8febf
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-10-06 07:09:53 -0300

    refactor(#41): remove arquivos da estrutura antiga
    - Remove controllers antigos não organizados
    - Remove DTOs antigos desorganizados
    - Remove entidades, repositories e services antigos
    - Limpa estrutura legada pós-refatoração

--- api/src/main/java/com/neohorizon/api/repository/DimProjetoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neohorizon.api.repository;
-
-import com.neohorizon.api.entity.DimProjeto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DimProjetoRepository extends JpaRepository<DimProjeto, Long>  {
-
-}
