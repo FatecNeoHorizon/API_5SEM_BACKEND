df752236ee2eb429ae3f46e35367355fd786da27
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-10-06 07:05:21 -0300

    refactor(#41): reorganiza repositórios de dimensão
    - Move repositórios de dimensão para pacote específico
    - Implementa padrão de acesso a dados dimensional
    - Mantem queries específicas por dimensão

---
+++ api/src/main/java/com/neohorizon/api/repository/dimensao/DimPeriodoRepository.java
@@ -0,0 +1,9 @@
+package com.neohorizon.api.repository.dimensao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neohorizon.api.entity.dimensao.DimPeriodo;
+
+public interface DimPeriodoRepository extends JpaRepository<DimPeriodo, Long>  {
+
+}
---
+++ api/src/main/java/com/neohorizon/api/repository/dimensao/DimTipoRepository.java
@@ -0,0 +1,9 @@
+package com.neohorizon.api.repository.dimensao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neohorizon.api.entity.dimensao.DimTipo;
+
+public interface DimTipoRepository extends JpaRepository<DimTipo, Long>  {
+
+}
