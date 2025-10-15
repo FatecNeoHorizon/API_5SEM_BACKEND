ede657a3269bb7e5325cb207d98ce1f3528eb68d
Gustavo Moura <gustavorobert84@gmail.com>
2025-10-15 01:15:46 -0300

    fix: corrigir imports duplicados no DimAtividadeController

--- api/src/main/java/com/neohorizon/api/controller/dimensao/DimAtividadeController.java
+++ api/src/main/java/com/neohorizon/api/controller/dimensao/DimAtividadeController.java
@@ -1,13 +1,5 @@
 package com.neohorizon.api.controller.dimensao;
 
-import com.neohorizon.api.dto.DimAtividadeDTO;
-import com.neohorizon.api.service.dimensao.DimAtividadeService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
