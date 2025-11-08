679bc258c0525a64d956451852461f3de3f296ab
cesarpelogia <pelogia.cesar@hotmail.com>
2025-11-08 18:38:00 -0300

    feat(#116): Adiciona configuração de segurança

---
+++ api/src/main/java/com/neohorizon/api/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.neohorizon.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.neohorizon.api.enums.RoleType;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/dim-*/**").permitAll()
+                .requestMatchers("/usuario/**").permitAll()
+                .requestMatchers("/**").hasRole(RoleType.MANAGER.name())
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> httpBasic.realmName("Neo Horizon"))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .addFilterBefore(
+                new JwtAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
