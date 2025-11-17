b2f5d4f951d1ce1c3c1dece43c94c91a40643179
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-11-16 21:09:37 -0300

    feat(#116): Adiciona classe mapper para usuário

--- api/src/main/java/com/neohorizon/api/mapper/UserMapper.java
+++ api/src/main/java/com/neohorizon/api/mapper/UserMapper.java
@@ -2,11 +2,17 @@ package com.neohorizon.api.mapper;
 
 import java.util.List;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import com.neohorizon.api.dto.usuario.UsuarioDTO;
 import com.neohorizon.api.entity.seguranca.Usuario;
 
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
     Usuario toEntity(UsuarioDTO usuarioDTO);
     UsuarioDTO toDTO(Usuario usuario);
 
