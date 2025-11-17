d2a7b080e348dc063d11b3a1ed12461613e3ef2f
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-11-16 21:11:00 -0300

    feat(#116): Atualiza repositório para usuário

--- api/src/main/java/com/neohorizon/api/repository/seguranca/UsuarioRepository.java
+++ api/src/main/java/com/neohorizon/api/repository/seguranca/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.neohorizon.api.repository.seguranca;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.neohorizon.api.entity.seguranca.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	Optional<Usuario> findByEmail(String email);
 }
