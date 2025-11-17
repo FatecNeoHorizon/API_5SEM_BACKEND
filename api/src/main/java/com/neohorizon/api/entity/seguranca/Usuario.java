6e6364b0087468af6f125ccb0f960c986de9c5ed
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-11-16 21:07:44 -0300

    feat(#116): Adiciona classe entidade para usuário

--- api/src/main/java/com/neohorizon/api/entity/seguranca/Usuario.java
+++ api/src/main/java/com/neohorizon/api/entity/seguranca/Usuario.java
@@ -9,14 +9,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.neohorizon.api.enums.RoleType;
 
-import lombok.Data;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -28,15 +30,17 @@ public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id", nullable = false)
     private Long id;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", unique = true,  nullable = false)
     private String email;
 
     @Column(name = "senha", nullable = false)
     private String senha;
 
     @Column(name = "cargo", nullable = false)
+    @Enumerated(EnumType.STRING) 
     private RoleType cargo;
 
 
