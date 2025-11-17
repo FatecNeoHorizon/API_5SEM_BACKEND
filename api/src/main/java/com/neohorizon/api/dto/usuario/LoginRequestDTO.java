5d8efc6b6a58fba99424327593c8751033a450d4
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-11-16 21:10:10 -0300

    feat(#116): Adiciona DTO para requisições de Login

---
+++ api/src/main/java/com/neohorizon/api/dto/usuario/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.neohorizon.api.dto.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+}
